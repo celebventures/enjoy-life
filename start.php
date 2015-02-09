712d1e6f58d7e9e41461f5ff8044fea65f116380
celebventures <celebrate.ventures@gmail.com>
2015-02-09 15:10:30 -0700

    Committed the first version of the starter files for enjoy-life project.

---
+++ start.php
@@ -0,0 +1,27 @@
+<?php
+	echo "hello";
+	// define the variables for the database
+
+	$db_host = "localhost";
+	$db_uname = "root";
+	$db_pass = "";
+	$db_name = "revel";
+	
+	// Other site related variables
+	echo "start config";
+	
+	// functions to connect to the database 
+	$db = @mysql_connect("$db_host", "$db_uname", "$db_pass");
+	if (!$db) {
+		echo "Not connected to the database";
+	}
+	else
+	{
+		echo "db connected";
+	}
+	
+	$dbselect = @mysql_select_db("$db_name", "$db");
+	
+	@mysql_close($db);
+
+?>
